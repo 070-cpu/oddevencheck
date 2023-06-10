06e27c2c07e8d01cb8372cdd4b8f8cfdbd7f6084
Debayan Chakraborty <debayanc032@gmail.com>
2023-06-10 11:22:55 +0530

    Add files via upload

---
+++ oddevencheck.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a=5;
+    if(a%2)
+    {
+        printf("Even number:");
+    }
+    else
+    {
+    printf("Odd number:");
+    }
+    getch();
+}
